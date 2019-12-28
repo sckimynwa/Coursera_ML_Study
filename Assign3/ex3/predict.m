f054bbcd8d76bc1587ea04de90403be1922bdcdc
user.email <tsb03672@naver.com>
2019-12-28 14:44:05 +0900

    neural network

--- Assign3/ex3/predict.m
+++ Assign3/ex3/predict.m
@@ -19,7 +19,17 @@ p = zeros(size(X, 1), 1);
 %       function can also return the index of the max element, for more
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
-%        
+%       
+
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+hypo = sigmoid(z3);
+[probability indices] = max(hypo');
+p = indices';
 
 % =========================================================================
 
